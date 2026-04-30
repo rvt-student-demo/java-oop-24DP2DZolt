a2c2c6cdf38bb272f4fed4f5a881d39cafeea176
God Kuzya <A242011DZ@rvt.lv>
2026-04-30 11:06:42 +0000

    Uzdevums "ToDo List", otrā daļa. Uzskāts UI.

--- src/main/java/rvt/todo_list/TodoList.java
+++ src/main/java/rvt/todo_list/TodoList.java
@@ -27,20 +27,5 @@ public class TodoList {
         
     }
 
-    public static void main(String[] args) {
-        TodoList list = new TodoList();
-        list.add("read the course material");
-        list.add("watch the latest fool us");
-        list.add("take it easy");
-        list.print();
-        list.remove(2);
-        list.print();
-        list.add("buy raisins");
-        list.print();
-        list.remove(1);
-        list.remove(1);
-        list.print();
-            }
-
 
 }
--- src/main/java/rvt/todo_list/UserInterface.java
+++ src/main/java/rvt/todo_list/UserInterface.java
@@ -1,5 +1,20 @@
 package rvt.todo_list;
 
+import java.util.*;
+
 public class UserInterface {
-    
+
+    private TodoList list;
+    private Scanner scanner;
+    public UserInterface(TodoList list, Scanner scanner){
+        this.list = list;
+        this.scanner = scanner;
+    }
+
+    public void start(){
+        while(true){
+            System.out.println("Choose operation: ");
+            System.out.println("1. ");
+        }
+    }
 }
