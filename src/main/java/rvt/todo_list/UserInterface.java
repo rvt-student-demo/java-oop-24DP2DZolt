edff263318ce36199a3d4d0cbf0b29c228ca67a1
A242011DZ <A242011DZ@rvt.lv>
2026-06-06 16:19:40 +0300

    Uzdevums "To do list". Izveidots CLI uzdevumam.

--- src/main/java/rvt/todo_list/UserInterface.java
+++ src/main/java/rvt/todo_list/UserInterface.java
@@ -6,15 +6,32 @@ public class UserInterface {
 
     private TodoList list;
     private Scanner scanner;
+    
     public UserInterface(TodoList list, Scanner scanner){
         this.list = list;
         this.scanner = scanner;
     }
 
-    public void start(){
-        while(true){
-            System.out.println("Choose operation: ");
-            System.out.println("1. ");
+    public void start() {
+        while (true) {
+            System.out.println("Command:");
+            String command = scanner.nextLine();
+            if (command.equals("stop")) {
+                break;
+            } else if (command.equals("list")) {
+                list.print();
+                continue;
+            } else if (command.equals("add")) {
+                System.out.print("To add: ");
+                String item = scanner.nextLine();
+                list.add(item);
+                continue;
+            } else if (command.equals("remove")) {
+                System.out.print("Which one is removed? ");
+                int item = Integer.valueOf(scanner.nextLine());
+                list.remove(item);
+                continue;
+            }
         }
     }
 }
